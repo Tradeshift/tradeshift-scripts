4a5b3438edff84e90d986cc20ae7ea3bbf635d97
Jacob Wejendorp <jwe@tradeshift.com>
2019-10-25 16:23:21 +0200

    fix(build): remove the ignored-but-copied files from the built output

--- src/scripts/build/babel.js
+++ src/scripts/build/babel.js
@@ -1,34 +1,51 @@
-const path = require('path')
-const spawn = require('cross-spawn')
-const rimraf = require('rimraf')
-const {hasPkgProp, fromRoot, resolveBin, hasFile} = require('../../utils')
+const path = require('path');
+const spawn = require('cross-spawn');
+const yargsParser = require('yargs-parser');
+const rimraf = require('rimraf');
+const glob = require('glob');
+const { hasPkgProp, fromRoot, resolveBin, hasFile } = require('../../utils');
 
-const args = process.argv.slice(2)
-const here = p => path.join(__dirname, p)
+const args = process.argv.slice(2);
+const here = p => path.join(__dirname, p);
+
+const parsedArgs = yargsParser(args);
 
 const useBuiltinConfig =
-  !args.includes('--presets') && !hasFile('.babelrc') && !hasPkgProp('babel')
-const config = useBuiltinConfig
-  ? ['--presets', here('../../config/babelrc.js')]
-  : []
+	!args.includes('--presets') &&
+	!hasFile('.babelrc') &&
+	!hasFile('.babelrc.js') &&
+	!hasFile('babel.config.js') &&
+	!hasPkgProp('babel');
+const config = useBuiltinConfig ? ['--presets', here('../../config/babelrc.js')] : [];
 
-const ignore = args.includes('--ignore')
-  ? []
-  : ['--ignore', '__tests__,__mocks__,spec.js']
+const builtInIgnore = '**/__tests__/**,**/__mocks__/**';
 
-const copyFiles = args.includes('--no-copy-files') ? [] : ['--copy-files']
+const ignore = args.includes('--ignore') ? [] : ['--ignore', builtInIgnore];
 
-const useSpecifiedOutDir = args.includes('--out-dir')
-const outDir = useSpecifiedOutDir ? [] : ['--out-dir', 'dist']
+const copyFiles = args.includes('--no-copy-files') ? [] : ['--copy-files'];
+
+const useSpecifiedOutDir = args.includes('--out-dir');
+const builtInOutDir = 'dist';
+const outDir = useSpecifiedOutDir ? [] : ['--out-dir', builtInOutDir];
 
 if (!useSpecifiedOutDir && !args.includes('--no-clean')) {
-  rimraf.sync(fromRoot('dist'))
+	rimraf.sync(fromRoot('dist'));
 }
 
 const result = spawn.sync(
-  resolveBin('@babel/cli', {executable: 'babel'}),
-  [...outDir, ...copyFiles, ...ignore, ...config, 'src'].concat(args),
-  {stdio: 'inherit'}
-)
+	resolveBin('@babel/cli', { executable: 'babel' }),
+	[...outDir, ...copyFiles, ...ignore, ...config, 'src'].concat(args),
+	{ stdio: 'inherit' }
+);
 
-process.exit(result.status)
+// because babel will copy even ignored files, we need to remove the ignored files
+const pathToOutDir = fromRoot(parsedArgs.outDir || builtInOutDir);
+const ignoredPatterns = (parsedArgs.ignore || builtInIgnore)
+	.split(',')
+	.map(pattern => path.join(pathToOutDir, pattern));
+const ignoredFiles = ignoredPatterns.reduce((all, pattern) => [...all, ...glob.sync(pattern)], []);
+ignoredFiles.forEach(ignoredFile => {
+	rimraf.sync(ignoredFile);
+});
+
+process.exit(result.status);
