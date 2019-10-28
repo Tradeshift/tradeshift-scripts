0fa6a25f8799e9226cee93a143c3248d593c2304
Jacob Wejendorp <jwe@tradeshift.com>
2019-10-28 21:30:17 +0100

    feat(eslint): use eslint-config-tradeshift/jest config by default

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -3,18 +3,7 @@ const { ifAnyDep } = require('../utils');
 module.exports = {
 	extends: [
 		require.resolve('eslint-config-tradeshift'),
-		// require.resolve('eslint-config-tradeshift/jest'),
+		require.resolve('eslint-config-tradeshift/jest'),
 		ifAnyDep('react', 'plugin:react/recommended')
-		// ifAnyDep('react', require.resolve('eslint-config-kentcdodds/react')),
-	].filter(Boolean),
-	plugins: ['jest', ifAnyDep('react', 'react')].filter(Boolean),
-	rules: {
-		'jest/no-disabled-tests': 1,
-		'jest/no-focused-tests': 2,
-		'jest/no-identical-title': 2,
-		'jest/valid-expect': 2
-	},
-	env: {
-		'jest/globals': true
-	}
+	].filter(Boolean)
 };
