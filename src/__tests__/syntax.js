7a6a9deadfb7c270ccc9ba1b0c1a7c272e56c325
Corneliu S <cos@tradeshift.com>
2019-11-06 15:32:40 +0100

    fix: add language plugin syntax tests

--- src/__tests__/syntax.js
+++ src/__tests__/syntax.js
@@ -6,4 +6,23 @@ describe('jest', () => {
 			expect(data.name).toEqual(pkg.name);
 		});
 	});
+	it('supports optional chaining operator', () => {
+		const o = { a: { b: { c: 'see' } } };
+		expect(o?.a?.b?.c).toEqual('see');
+		expect(o?.a?.z?.c).toEqual(undefined);
+	});
+	it('supports nullish-coalescing-operator', () => {
+		const o = { a: 'foo', c: null };
+		expect(o.b ?? 'default').toEqual('default');
+		expect(o.c ?? 'default').toEqual('default');
+	});
+	it('supports class properties', () => {
+		class ClassExample {
+			instanceProperty = 'bork';
+			static staticProperty = 'babelIsCool';
+		}
+		const i = new ClassExample();
+		expect(i.instanceProperty).toEqual('bork');
+		expect(ClassExample.staticProperty).toEqual('babelIsCool');
+	});
 });
