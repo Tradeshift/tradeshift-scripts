3cd45c5283b2a77e3c8cbd58322a3f6ee5ffa3e0
Jacob Wejendorp <jwe@tradeshift.com>
2020-09-25 17:43:03 +0200

    fix(deps): update lint-staged from 8.2.1 to 10.4.0

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -4,14 +4,10 @@ const kcdScripts = resolveKcdScripts();
 const doctoc = resolveBin('doctoc');
 
 module.exports = {
-	concurrent: false,
-	linters: {
-		'**/*.+(js|jsx|json|less|css|ts|tsx)': [
-			`${kcdScripts} format`,
-			`${kcdScripts} lint`,
-			`${kcdScripts} test --findRelatedTests`,
-			'git add',
-		],
-		'README.md': [`${doctoc} --maxlevel 2 --notitle`, 'git add'],
-	},
+	'**/*.+(js|jsx|json|less|css|ts|tsx)': [
+		`${kcdScripts} format`,
+		`${kcdScripts} lint`,
+		`${kcdScripts} test --findRelatedTests`,
+	],
+	'README.md': [`${doctoc} --maxlevel 2 --notitle`],
 };
