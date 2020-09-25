18967dd012bbf2f248445caa2643ec9c3c7bf1c0
Jacob Wejendorp <jwe@tradeshift.com>
2020-09-25 17:43:03 +0200

    fix(deps): update lint-staged from 8.2.1 to 10.4.0

--- src/scripts/precommit/lint-staged.js
+++ src/scripts/precommit/lint-staged.js
@@ -1,38 +1,28 @@
 const path = require('path');
-const resolve = require('resolve');
-const { hasPkgProp, hasFile } = require('../../utils');
+const spawn = require('cross-spawn');
+const { hasPkgProp, hasFile, resolveBin } = require('../../utils');
 
-const useBuiltinConfig =
+const here = (p) => path.join(__dirname, p);
+const hereRelative = (p) => here(p).replace(process.cwd(), '.');
+
+const args = process.argv.slice(2);
+
+const useBuiltInConfig =
+	!args.includes('--config') &&
 	!hasFile('.lintstagedrc') &&
 	!hasFile('lint-staged.config.js') &&
 	!hasPkgProp('lint-staged');
 
-const lintStagedPath = require.resolve('lint-staged');
-const cosmiconfigPath = resolve.sync('cosmiconfig', {
-	basedir: path.dirname(lintStagedPath),
-});
+const config = useBuiltInConfig
+	? ['--config', hereRelative('../../config/lintstagedrc.js')]
+	: [];
 
-// lint-staged uses cosmiconfig to find its configuration
-// and it has no other way to provide config
-// (via a node API or command-line flag)
-// So, we're doing this require cache magic to provide our own
-// config so folks don't have to have that in their package.json
-function fakeCosmiconfig(...args) {
-	if (args[0] === 'lint-staged') {
-		return {
-			search() {
-				return Promise.resolve({
-					config: require('../../config/lintstagedrc'),
-				});
-			},
-		};
-	} else {
-		return require(cosmiconfigPath)(...args);
-	}
+const lintStagedResult = spawn.sync(
+	resolveBin('lint-staged'),
+	[...config, ...args],
+	{ stdio: 'inherit' },
+);
+
+if (lintStagedResult.status !== 0) {
+	process.exit(lintStagedResult.status);
 }
-
-if (useBuiltinConfig) {
-	require.cache[cosmiconfigPath] = { exports: fakeCosmiconfig };
-}
-
-require(lintStagedPath);
